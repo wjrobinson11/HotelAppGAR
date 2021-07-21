294cc135585f1966a44b4365e3d2c553785dd845
wjrobinson11 <wjrobinson11@gmail.com>
2021-07-21 15:10:06 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# README
+
+A hotel app sandbox project for Getaroom candidates to use to submit code assignments. Built off of the Jumpstart Rails template https://github.com/excid3/jumpstart.
+
+* Ruby version 2.7.0
+* Rails version 6.0.3.4
+* Database Sqlite3
+
+To set up, clone the repo, when inside the project run the following:
+
+* bundle install
+* bundle exec rake db:create
+* bundle exec rake db:migrate
+* bundle exec rake db:seed
+* rails server
+
+And you should be good to go on localhost:3000
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Hotel.create([
+  { name: "Comfort Inn & Suites Market Ctr", location: "7138 N Stemmons Fwy, Dallas, TX, 75247" },
+  { name: "Hilton Anatole", location: "2201 Stemmons Freeway, Dallas, TX, 75207" },
+  { name: "Hotel ZaZa", location: "2332 Leonard St, Dallas, TX, 75201" },
+  { name: "The Stoneleigh Hotel & Spa", location: "2927 Maple Ave, Dallas, TX, 75201" },
+  { name: "Crap Hotel", location: "123 N Main St, Dallas, TX, 75201" },
+  { name: "Hotel Indigo Test Hotel", location: "1933 Main St, Dallas, TX, 75201" },
+  { name: "Renaissance Dallas Addison", location: "15201 Dallas Parkway, Dallas, TX, 75001" },
+  { name: "Dallas Symphony Orchestra", location: "2301 Flora Street, Dallas, TX, 75201" },
+  { name: "Warwick Melrose Hotel Dallas - Demo", location: "3015 Oak Lawn Avenue, Dallas, TX, 75219" },
+  { name: "Econo Lodge Airport I-35 North Hotel", location: "2275 Valley View Lane, Dallas, TX, 75234" },
+])
