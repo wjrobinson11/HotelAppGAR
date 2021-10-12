2aff4192afef2bf9420b373025de9f5f5c069278
Kittu <kittu@Kittus-MacBook-Pro.local>
2021-10-11 22:37:22 -0500

    Room type migration, model and respective specs added

---
+++ db/migrate/20211012032320_room_types.rb
@@ -0,0 +1,10 @@
+class RoomTypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :room_types do |t|
+      t.string :name
+      t.boolean :available
+      t.belongs_to :hotel
+      t.timestamps
+    end
+  end
+end
